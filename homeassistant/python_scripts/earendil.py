758cecc7a0ac9088ea4b7528fcdfd72d2549bbb9
makadaw <makadaw@gmail.com>
2019-12-02 17:03:39 +0000

    Add brightness button support

--- homeassistant/python_scripts/earendil.py
+++ homeassistant/python_scripts/earendil.py
@@ -9,11 +9,13 @@ OP_TURN_ON = "turn_on"
 OP_TURN_OFF = "turn_off"
 OP_DIM_UP = "dim_up"
 OP_DIM_DOWN = "dim_down"
+OP_DIM_HOLD_UP = "dim_hold_up"
 
 # Get params
 entity_id = data.get("entity_id")
 op = data.get(ARG_OPERATION, OP_TOGGLE)
-level = data.get(ARG_LEVEL, 5)
+level = data.get(ARG_LEVEL, 10)
+transition = data.get("transition", 1)
 
 def min(a,b):
   return a if a < b else b
@@ -23,13 +25,13 @@ def max(a,b):
 
 if entity_id is not None:
   if op == OP_TURN_ON:
-    data = {"entity_id": entity_id, "brightness": 255, "transition": 1}
+    data = {"entity_id": entity_id, "brightness": 255, "transition": transition}
     hass.services.call("light", "turn_on", data)
   elif op == OP_TURN_OFF:
-    data = {"entity_id": entity_id, "transition": 1}
+    data = {"entity_id": entity_id, "transition": transition}
     hass.services.call("light", "turn_off", data)
   elif op == OP_TOGGLE:
-    data = {"entity_id": entity_id, "brightness": 255, "transition": 1}
+    data = {"entity_id": entity_id, "brightness": 255, "transition": transition}
     hass.services.call("light", "toggle", data, False)
   elif op == OP_DIM_UP or op == OP_DIM_DOWN:
     # Get current brightness value
@@ -38,8 +40,43 @@ if entity_id is not None:
     brightness = min(current+level, 100) if op == OP_DIM_UP else max(current-level, 0)
   
     logger.info("Set light brightness level to {}".format(brightness))
-    data = {"entity_id": entity_id, "brightness": round(brightness * 2.55), "transition": 1}
+    data = {"entity_id": entity_id, "brightness": round(brightness * 2.55), "transition": transition}
     hass.services.call("light", "turn_on", data, False)
+  elif op == OP_DIM_HOLD_UP:
+    # Long press actions
+    exit_service = False
+    attribute_id = data.get("light_attribute", "brightness")
+    input_boolean = data.get("input_boolean")
+    delay = data.get("delay", 350)
+
+    attribute_mapping = {
+      "brightness": {"min": 1, "max": 255},
+      "color_temp": {"min": 153, "max": 500},
+    }
+    
+    # Start dim loop
+    while True:
+      switch_input = hass.states.get(input_boolean).state
+      if switch_input == "off":
+        break
+      light = hass.states.get(entity_id)
+      
+      attr_value = light.attributes[attribute_id]
+      attr_value = attr_value + level
+      if attr_value < attribute_mapping[attribute_id]["min"]:
+        attr_value = attribute_mapping[attribute_id]["min"]
+        exit_service = True
+      elif attr_value > attribute_mapping[attribute_id]["max"]:
+        attr_value = attribute_mapping[attribute_id]["max"]
+        exit_service = True
+
+      service_data = {"entity_id": entity_id, attribute_id: attr_value}
+      logger.info("Dim hold set level {}".format(service_data))
+      hass.services.call("light", "turn_on", service_data)
+      if exit_service:
+        break
+      time.sleep(delay / 1000)
+
 else:
   logger.warning("No entity_id provided")
 
